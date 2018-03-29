0462a2c950fc3169ab7e003297efdb59f562482c
mTresk <djtresk@gmail.com>
2018-03-29 15:24:18 +0000

    Update firmware flashing extension for OP5T
    Change-Id: Ided87a80b0799e56034706cd29f4885b65c5639d

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -20,32 +20,61 @@ include $(CLEAR_VARS)
 target_firmware_path := $(ANDROID_BUILD_TOP)/vendor/oneplus/oneplus5/firmware_images/
 
 
+# Oneplus 5
 # static_nvbk file
-$(call add-firmware-file,$(target_firmware_path)/static_nvbk.bin)
+$(call add-firmware-file,$(target_firmware_path)/static_nvbk.bin-op5)
 # Radio fusion file
-$(call add-firmware-file,$(target_firmware_path)/NON-HLOS.bin)
+$(call add-firmware-file,$(target_firmware_path)/NON-HLOS.bin-op5)
 # Bluetooth file
-$(call add-firmware-file,$(target_firmware_path)/BTFM.bin)
+$(call add-firmware-file,$(target_firmware_path)/BTFM.bin-op5)
 # rpm image
-$(call add-firmware-file,$(target_firmware_path)/rpm.mbn)
+$(call add-firmware-file,$(target_firmware_path)/rpm.mbn-op5)
 # pmic file
-$(call add-firmware-file,$(target_firmware_path)/pmic.elf)
+$(call add-firmware-file,$(target_firmware_path)/pmic.elf-op5)
 # tz version image
-$(call add-firmware-file,$(target_firmware_path)/tz.mbn)
+$(call add-firmware-file,$(target_firmware_path)/tz.mbn-op5)
 # hyp image
-$(call add-firmware-file,$(target_firmware_path)/hyp.mbn)
+$(call add-firmware-file,$(target_firmware_path)/hyp.mbn-op5)
 # adspso file
-$(call add-firmware-file,$(target_firmware_path)/adspso.bin)
+$(call add-firmware-file,$(target_firmware_path)/adspso.bin-op5)
 # cmnlib image
-$(call add-firmware-file,$(target_firmware_path)/cmnlib.mbn)
+$(call add-firmware-file,$(target_firmware_path)/cmnlib.mbn-op5)
 # cmnlib64 image
-$(call add-firmware-file,$(target_firmware_path)/cmnlib64.mbn)
+$(call add-firmware-file,$(target_firmware_path)/cmnlib64.mbn-op5)
 # devcfg image
-$(call add-firmware-file,$(target_firmware_path)/devcfg.mbn)
+$(call add-firmware-file,$(target_firmware_path)/devcfg.mbn-op5)
 # keymaster image
-$(call add-firmware-file,$(target_firmware_path)/keymaster.mbn)
+$(call add-firmware-file,$(target_firmware_path)/keymaster.mbn-op5)
 # xbl file
-$(call add-firmware-file,$(target_firmware_path)/xbl.elf)
+$(call add-firmware-file,$(target_firmware_path)/xbl.elf-op5)
+
+# Oneplus 5t
+# Logo
+$(call add-firmware-file,$(target_firmware_path)/logo.bin-op5t)
+# Radio fusion file
+$(call add-firmware-file,$(target_firmware_path)/NON-HLOS.bin-op5t)
+# Bluetooth file
+$(call add-firmware-file,$(target_firmware_path)/BTFM.bin-op5t)
+# rpm image
+$(call add-firmware-file,$(target_firmware_path)/rpm.mbn-op5t)
+# pmic file
+$(call add-firmware-file,$(target_firmware_path)/pmic.elf-op5t)
+# tz version image
+$(call add-firmware-file,$(target_firmware_path)/tz.mbn-op5t)
+# hyp image
+$(call add-firmware-file,$(target_firmware_path)/hyp.mbn-op5t)
+# adspso file
+$(call add-firmware-file,$(target_firmware_path)/adspso.bin-op5t)
+# cmnlib image
+$(call add-firmware-file,$(target_firmware_path)/cmnlib.mbn-op5t)
+# cmnlib64 image
+$(call add-firmware-file,$(target_firmware_path)/cmnlib64.mbn-op5t)
+# devcfg image
+$(call add-firmware-file,$(target_firmware_path)/devcfg.mbn-op5t)
+# keymaster image
+$(call add-firmware-file,$(target_firmware_path)/keymaster.mbn-op5t)
+# xbl file
+$(call add-firmware-file,$(target_firmware_path)/xbl.elf-op5t)
 
 # Unset local variable
 target_firmware_path :=
